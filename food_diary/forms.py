f47f3ec0fdef58b48f3971a06cb02a738ce90f9b
davidjmolina55-lgtm <d.molina55@hotmail.com>
2025-12-05 12:38:00 +0000

    Remove user field from form

--- food_diary/forms.py
+++ food_diary/forms.py
@@ -6,12 +6,12 @@ from .models import Food
 class FoodForm(forms.ModelForm):
     class Meta:
         model = Food
-        fields = ['name', 'date', 'meal_type', 'user']
+        # Do not expose the owner in the form; set it server-side in views
+        fields = ['name', 'date', 'meal_type']
         widgets = {
             'date': forms.DateInput(
                 attrs={'type': 'date', 'class': 'form-control'}
             ),
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'meal_type': forms.Select(attrs={'class': 'form-select'}),
-            'user': forms.Select(attrs={'class': 'form-select'}),
         }
